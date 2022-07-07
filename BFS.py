415184ca1b72d3c01a3c5b34c3670742b4c25da3
arialghifari <arialghifari99@gmail.com>
2022-07-07 14:38:55 +0800

    Add error handling

--- BFS.py
+++ BFS.py
@@ -1,7 +1,7 @@
 from queue import Queue
 
-# initialize node
-adj_list = {
+# Initialize node
+node_list = {
     "A": ["B", "C"],
     "B": ["D", "G"],
     "C": ["E", "F"],
@@ -12,49 +12,62 @@ adj_list = {
     "H": [],
 }
 
-# initialize data structure
+# Initialize data structure
 visited = {}
 level = {}
 parent = {}
 bfs_traversal_output = []
 queue = Queue()
 
-# initialize node
-for node in adj_list.keys():
+# Initialize node
+for node in node_list.keys():
     visited[node] = False
     parent[node] = None
     level[node] = -1
 
+# Start traversing node from source node
 s = "A"  # source node
-visited[s] = True
-level[s] = 0
-queue.put(s)  # put node to queue
 
-# start traversing node
-while not queue.empty():
-    node = queue.get()  # get node from queue
-    bfs_traversal_output.append(node)
+try:
+    visited[s] = True
+    level[s] = 0
+    queue.put(s)  # put node to queue
 
-    # start traversing each child node
-    for child_node in adj_list[node]:
-        if not visited[child_node]:
-            visited[child_node] = True
-            parent[child_node] = node
-            level[child_node] = level[node] + 1
-            queue.put(child_node)  # add child node to queue
+    while not queue.empty():
+        node = queue.get()  # get first node from queue
+        bfs_traversal_output.append(node)  # put visited node to traversal
 
-# shortest path from source node to target node
+        # start traversing each child node
+        for child_node in node_list[node]:
+            if not visited[child_node]:
+                visited[child_node] = True
+                parent[child_node] = node
+                level[child_node] = level[node] + 1
+                queue.put(child_node)  # add child node to queue
+except:
+    print("Source node " + s + " is not available")
+    exit()
+
+
+# Find the shortest path from source node to target node
 t = "H"  # target node
-t_print = t
+t_for_print = t
 
-path = []
-while t is not None:  # from target node look for its parent
-    path.append(t)
-    t = parent[t]
-path.reverse()
+try:
+    path = []
+    while t is not None:  # from target node look for its parent
+        path.append(t)
+        t = parent[t]
+    path.reverse()
 
-print("Shortest step from A to H is:")
-print(str(level[t_print]) + " steps")
-print("------------------------------------------")
-print("Shortest path is: ")
-print(path)
+    print("BFS Traversal:")
+    print(bfs_traversal_output)
+    print("------------------------------------------")
+    print("Shortest step:")
+    print(str(level[t_for_print]) + " steps")
+    print("------------------------------------------")
+    print("Shortest path: ")
+    print(path)
+except:
+    print("Target node " + t + " is not available")
+    exit()
